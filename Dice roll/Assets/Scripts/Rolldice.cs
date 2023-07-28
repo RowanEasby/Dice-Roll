cf70f47c6f714ffd4d3af04f028899f80d236bf4
ROWAN EASBY <re280275@falmouth.ac.uk>
2023-07-28 12:01:32 +0100

    Fix commit
    Commits were misaligned and I kind of just want things to match. Not sure what changes have been made but nothing major

--- Dice roll/Assets/Scripts/Rolldice.cs
+++ Dice roll/Assets/Scripts/Rolldice.cs
@@ -6,6 +6,7 @@ using Cinemachine;
 
 public class Rolldice : MonoBehaviour
 {
+    //Declarations
     public GameObject die;
     public Rigidbody rb;
 
@@ -15,13 +16,13 @@ public class Rolldice : MonoBehaviour
 
     public TMP_Text tm;
 
-
     //Used to determine when the die has stopped moving
     float speed;
     float realX;
     float realZ;
     float rotX;
     float rotZ;
+
 
     public CinemachineVirtualCamera LookCam;
     public CinemachineVirtualCamera TopCam;
@@ -37,8 +38,11 @@ public class Rolldice : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
         //determines the die's speed
         speed = rb.velocity.magnitude;
+
 
         //Determines the Die's current rotational position to calculate what number has been rolled
         realX = Mathf.Round(transform.localEulerAngles.x);
@@ -54,34 +58,21 @@ public class Rolldice : MonoBehaviour
             rotZ = 0;
         }
 
-
         if (Input.GetKeyDown("space"))
         {
             die.transform.position = new Vector3(0, 25, 3); //Moves the die up
             Vector3 dieRotation = new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)); //Creates a Vector3 with random rotations
             die.transform.eulerAngles = dieRotation; //Applies the above Vector3 to the Die
             rb.velocity = new Vector3(Random.Range(-25,25), -10 , Random.Range(-25, 25));//Gives the Die random velocities on the X and Z axis
-        }
-
-
-        if (Input.GetKeyDown(KeyCode.J))
-        {
+            
             FollowCam();
         }
 
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            OverheadCam();
-        }
-
         //If the Die isn't moving
-        if (speed < 0.1)
+        if (speed < 0.05)
         {
             Rolled();
         }
-
-
-
     }
 
     public void Rolled()
@@ -113,6 +104,10 @@ public class Rolldice : MonoBehaviour
 
         tm.text = Number; //Changes the UI text to show the die roll result
 
+        if (speed < 0.05)
+        {
+            OverheadCam();
+        }
     }
 
     public void OverheadCam()
@@ -130,11 +125,14 @@ public class Rolldice : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+
+    //Plays particles when the die hit's the floor
         particle.Play(true);
     }
 
     private void OnCollisionExit(Collision collision)
     {
+    //Turns of the particles when the die leaves the floor
         particle.Play(false);
     }
 }
